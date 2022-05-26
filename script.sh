7a03090e5519e73bace2fd581b13ed19a1dae926
Ken Q <duckhang.quach@gmail.com>
2022-05-26 21:49:22 +1000

    add script file to run experiment sequentially

---
+++ script.sh
@@ -0,0 +1,32 @@
+python test.py --task traffic_state_pred --model DCRNN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model DCRNN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model STGCN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model STGCN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model GWNET --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model GWNET --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model MTGNN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model MTGNN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model TGCN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model TGCN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model TGCLSTM --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model TGCLSTM --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model GMAN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model GMAN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model GTS --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model GTS --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model STAGGCN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model STAGGCN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model HGCN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model HGCN --dataset PEMS_BAY_0.5
+
+python test.py --task traffic_state_pred --model STTN --dataset PEMS_BAY
+python test.py --task traffic_state_pred --model STTN --dataset PEMS_BAY_0.5
